c6ceb25957c5b40236fdbfd0c2eed88bca8713bc
mhd-humraz <muhammadhamraz201@gmail.com>
2026-04-18 11:16:51 +0530

    set all files

--- hand_tracker/hand/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HandConfig(AppConfig):
-    name = 'hand'
--- test/hand_web/hand_web/asgi.py
+++ test/hand_web/hand_web/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hand_tracker project.
+ASGI config for hand_web project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hand_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hand_web.settings')
 
 application = get_asgi_application()
